ad383f79d2ea1c2ab4604cd7e54e39c3d77fb96b
MK <git-user@selectel.ru>
2023-12-15 19:26:01 +0300

    добавлены выполненные задачи

---
+++ ex_01/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
+// Использовать рекурсию, не использовать циклы.
+
+Console.Write("Введите значение M:\t");
+int start = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите значение N:\t");
+int end = Convert.ToInt32(Console.ReadLine());
+
+string OutputFromTo(int start, int end)
+{
+    if (start == end) return end.ToString();
+    return start + " " + OutputFromTo(start + 1, end);
+}
+Console.Write($"Печать чисел от {start} до {end}:\n{OutputFromTo(start, end)}");
---
+++ ex_03/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+Console.Write("Введите размер массива:\t");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[] CreateArray(int size)//создание массива
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+    }
+    return array;
+}
+int[] array = CreateArray(n);
+Console.WriteLine($"Исходный массив:\n {string.Join(" ", array)}");
+int i = 0;
+void ReverseArray(int[] array, int i)
+{
+
+    if (i < array.Length)
+    {
+        Console.Write($"{array[array.Length - i - 1]} ");
+        i++;
+        ReverseArray(array, i);
+    }
+}
+Console.WriteLine($"Перевернутый массив:");
+ReverseArray(array, i);
+
